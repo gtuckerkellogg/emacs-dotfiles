47989bf8d6b2b0b2a104418794b66bb52c0fd02b
Greg Tucker-Kellogg <dbsgtk@gmail.com>
2022-07-07 17:16:39 +0800

    Added org-modern

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
   (exec-path-from-shell-initialize))
 
 (use-package org)
+(org-reload)
 
 
 (org-babel-load-file (expand-file-name (concat (getenv "USER") ".org") user-emacs-directory))
@@ -35,3 +36,16 @@
 (put 'dired-find-alternate-file 'disabled nil)
 
 (provide 'init);; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
